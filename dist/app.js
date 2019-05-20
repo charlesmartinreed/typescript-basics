aa02f6f0f95e4bbfe74bccd5fcce12b76b7a4b4f
charlesmartinreed <charlesmartinreed@icloud.com>
2019-05-20 16:09:22 -0500

    Demonstrate using custom TS types in express.

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,15 @@
 "use strict";
-const greeting = "hello";
-const numbers = [2, 3, 5];
-const moreNumbers = [5, 6, 9];
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// import available because we're having the ts compiler compiled down to es2015
+const express_1 = __importDefault(require("express"));
+// when implementing the package @types/express, we get the above Express Types
+const app = express_1.default();
+const add = (a, b) => a + b;
+app.get("/", (req, res, next) => {
+    console.log(add(5, 5));
+    res.send("Hello, world!");
+});
+app.listen(5000, () => console.log("Server running"));
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,14 @@
-const greeting: string = "hello";
-const numbers: number[] = [2, 3, 5];
-const moreNumbers: Array<number> = [5, 6, 9];
+// import available because we're having the ts compiler compiled down to es2015
+import express, { Application, Request, Response, NextFunction } from "express";
+
+// when implementing the package @types/express, we get the above Express Types
+const app: Application = express();
+
+const add = (a: number, b: number): number => a + b;
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  console.log(add(5, 5));
+  res.send("Hello, world!");
+});
+
+app.listen(5000, () => console.log("Server running"));
